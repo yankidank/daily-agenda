1c86b6902194f9c016701b86b4d679851f7e2cf7
Eric Heikkinen <18619+yankidank@users.noreply.github.com>
2020-01-29 15:35:34 -0800

    Pressing return on the keyboard when editing the input field now saves it

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # calendar
 Day planner that saves data to localStorage
-Makes use of jQuery 3.4.1
+Makes use of [jQuery 3.4.1](https://code.jquery.com/)
 
+https://yankidank.github.io/daily-agenda/
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,10 @@
 const hours =  ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23'];
 var localValues = new Array(24);
 var storedValues = new Array(24);
+var saveStatus = false;
 
 //window.localStorage.clear();
-storedValues = JSON.parse(window.localStorage.getItem("storedValues"));
+storedValues = JSON.parse(window.localStorage.getItem("storedValues"))
 if (storedValues === null){
   storedValues = ['Sample 1', 'Sample 2', 'Sample 3', 'Sample 4','','','','','','','','','','','','','','','','','','','','']
   //console.log('no values stored yet')
@@ -20,21 +21,38 @@ function renderHour(item, index) {
   //console.log(savedText)
 
   // HTML for each hour
-  $('#calendar').append('<div class="hour_wrapper"><div class="hour" id="hour_'+item+'"><div class="hour_item">'+item+'</div></div><div class="input"><div class="input_item field"><input class="input_field" type="text" name="hour_input" id="input_'+item+'" value="'+savedText+'"></div></div><div class="save" id="save_'+item+'"><div class="save_item button_green" id="save_'+item+'">Save</div></div></div>');
+  $('#calendar').append('<div class="hour_wrapper"><div class="hour" id="hour_'+item+'"><div class="hour_item">'+item+'</div></div><div class="input"><div class="input_item field"><input class="input_field" type="text" name="hour_input" id="input_'+item+'" value="'+savedText+'"></div></div><div class="save" id="save_'+item+'"><div class="save_item button_green" id="saveText_'+item+'">Save</div></div></div>');
   
-  // Detect Save clicks 
-  // TODO: Detect input field submit (enter on keyboard)
-  $( "#save_"+item ).click(function() {
+  function savetoLocal(){
     // Assign input field value to a variable
-    inputValue = $("#input_"+item).val();
+    inputValue = $("#input_"+item).val()
     //console.log(inputValue + ' for ' +item)
-    storedValues.splice(index, 1, inputValue);
-    //console.log(storedValues);
-    localStorage.setItem("storedValues", JSON.stringify(storedValues));
+    storedValues.splice(index, 1, inputValue)
+    //console.log(storedValues)
+    localStorage.setItem("storedValues", JSON.stringify(storedValues))
+  }
+  function saveFunction(){
+    // Detect Save clicks 
+    $( "#save_"+item ).click(function() {
+      savetoLocal()
+    });
+    // Detect input field submit (enter on keyboard)
+    $("#input_"+item ).on('keyup', function (e) {
+      if (e.keyCode === 13) {
+        savetoLocal()
+      }
+    });
+    return saveStatus
+  }
+  saveFunction()
+  //console.log(saveStatus)
+  $("#input_"+item).on("input", function() {
+    //console.log(saveStatus + " : " + this.value);
+    $("#saveText_"+item).css( "background", "red" );
+    if (saveFunction() === true){
+      $("#saveText_"+item).css( "background", "green" );
+    }
   });
 
-
 }
-
-
 
