14298c4baeb4f26091f3a13cb27ba9919be9218e
Eric Heikkinen <18619+yankidank@users.noreply.github.com>
2020-01-30 04:10:03 -0800

    Cleaning up js code

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,30 +5,18 @@ var storedValues = Array.apply(0, Array(24)).map(function(){return '';});
 var currentHour = Number(moment().format('H'));
 var AMPM = '';
 var itemTwelve;
-
 $('h1').append(moment().format('dddd, MMMM Do'));
-
 function clearAgenda(){
   window.localStorage.clear();
   location.reload();
 }
-
 storedValues = JSON.parse(window.localStorage.getItem("storedValues"))
 if (storedValues === null){
    storedValues = Array.apply(0, Array(24)).map(function(){return '';});
-  //storedValues = ['','','','','','','','','','Begin working','Work on presentation','Study new skill','Lunch break','','','','','','','','','','','']
-  //console.log('no values stored yet')
-  //console.log(storedValues)
-} else {
-  //console.log('values from localStorage')
-  //console.log(storedValues)
 }
-
 hours.forEach(renderHour);
-
 function renderHour(item, index) {
   var savedText = storedValues[item]
-  //console.log(savedText)
   if (item < 12){
     AMPM = 'AM'
   } else {
@@ -39,43 +27,14 @@ function renderHour(item, index) {
   } else {
     itemTwelve = item
   }
-
   // HTML for each hour
   $('#calendar').append('<div class="hour_wrapper"><div class="hour" id="hour_'+item+'"><div class="hour_num"><div class="hour_item"></div>'+itemTwelve+' '+AMPM+'</div></div><div class="input"><div class="input_item field"><input class="input_field" type="text" name="hour_input" id="input_'+item+'" value="'+savedText+'"></div></div><div class="save" id="save_'+item+'"><div class="save_item" id="saveText_'+item+'"></div></div></div>');
-
   function savetoLocal(){
-    // Assign input field value to a variable
     inputValue = $("#input_"+item).val()
-    //console.log(inputValue + ' for ' +item)
     storedValues.splice(item, 1, inputValue)
-    //console.log(storedValues)
     localStorage.setItem("storedValues", JSON.stringify(storedValues))
   }
-  function saveFunction(){
-    /* 
-    // Detect Save clicks 
-    $( "#save_"+item ).click(function() {
-      savetoLocal()
-      $('#save_'+item).empty();
-      $('#save_'+item).prepend('<div class="checkWrapper"><img class="checkmark" src="./assets/img/checkmark.gif" /></div>')
-      setTimeout(function(){
-        $('#save_'+item).empty();
-      }, 1000);
-    });
-     */
-    // Detect input field submit (enter on keyboard)
-    $("#input_"+item ).on('keyup', function (e) {
-      if (e.keyCode === 13) {
-        savetoLocal()
-        $('#save_'+item).empty();
-        $('#save_'+item).prepend('<div class="checkWrapper"><img class="checkmark" src="./assets/img/checkmark.gif" /></div>')
-        setTimeout(function(){
-          $('#save_'+item).empty();
-        }, 1000);
-      }
-    });
-  }
-  // Save automatically
+  // Save input automatically
   var timeoutId;
   $('.input_field').on('input propertychange change', function() {
     clearTimeout(timeoutId);
@@ -83,8 +42,18 @@ function renderHour(item, index) {
       savetoLocal();
     }, 600);
   });
-  // Check for manual save actions (button, enter)
-  saveFunction()
+  // Detect enter on keyboard
+  $("#input_"+item ).on('keyup', function (e) {
+    if (e.keyCode === 13) {
+      savetoLocal()
+      $('#save_'+item).empty();
+      $('#save_'+item).prepend('<div class="checkWrapper"><img class="checkmark" src="./assets/img/checkmark.gif" /></div>')
+      setTimeout(function(){
+        $('#save_'+item).empty();
+      }, 1000);
+    }
+  });
+  // Detect typing input
   $("#input_"+item).on("input", function() {
     savetoLocal();
     clearTimeout(timeoutId);
@@ -96,29 +65,24 @@ function renderHour(item, index) {
       }, 1000);
     }, 600);
   });
-  /*
   // If the user clicks outside of the input, display checkmark on save
-  $(document).mouseup(function(e){
-    
-      var container = $(".input_field");
-      if (!container.is(e.target) && container.has(e.target).length === 0) 
-      {
+/*   $(document).mouseup(function(e){
+    var container = $("#input_"+item);
+    if (!container.is(e.target) && container.has(e.target).length === 0){
+      $('#save_'+item).empty();
+      $('#save_'+item).prepend('<div class="checkWrapper"><img class="checkmark" src="./assets/img/checkmark.gif" /></div>')
+      setTimeout(function(){
         $('#save_'+item).empty();
-        $('#save_'+item).prepend('<div class="checkWrapper"><img class="checkmark" src="./assets/img/checkmark.gif" /></div>')
-        setTimeout(function(){
-          $('#save_'+item).empty();
-        }, 1000);
-      }
-  }); 
-  */
-
+      }, 1000);
+    }
+  });  */
   if(item < currentHour){
+    // Change the input field background color if the time has passed
     $("#input_"+item).css( "background", "rgb(250, 250, 249)" );
     $("#input_"+item).css( "color", "rgb(183, 183, 183)" );
   } else if(item <= currentHour){
+    // Display an indicator icon next to the current hour
     $("#hour_"+item+" .hour_item").css( "background", "rgb(0, 211, 119)" );
     $("#hour_"+item+" .hour_item").css( "border-radius", "50%" );
-    $("#hour_"+item+" .hour_item").css( "color", "#FFF" );
-    $("#hour_"+item+" .hour_item").css( "font-weight", "bold" );
   }
 }
